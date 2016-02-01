3927ba4f133545a864073cd1078665828b2dbad2
Luca Fabbri <luca@keul.it>
2016-02-01 15:17:25 +0100

    properly plugin initialization, with custom icon

--- CHANGES.rst
+++ CHANGES.rst
@@ -4,6 +4,9 @@ Changelog
 0.4 (unreleased)
 ------------------
 
+- properly plugin initialization, with custom icon
+  [keul]
+
 - fix: ``getPropertiesForUser`` uses principals ``_properties`` mapping now
   to map to the column used for ``zope_id`` id this is needed.
   [jensens]
--- src/pas/plugins/sqlalchemy/__init__.py
+++ src/pas/plugins/sqlalchemy/__init__.py
@@ -3,6 +3,8 @@ from Products.PluggableAuthService.PluggableAuthService import MultiPlugins
 from Products.PluggableAuthService.PluggableAuthService import \
     registerMultiPlugin
 import plugin
+from AccessControl.Permissions import add_user_folders
+import os
 
 plugins = set()
 
@@ -10,3 +12,11 @@ plugins = set()
 def initialize(context):
     if plugin.Plugin.meta_type not in MultiPlugins:
         registerMultiPlugin(plugin.Plugin.meta_type)
+        context.registerClass(
+                plugin.Plugin,
+                permission=add_user_folders,
+                icon=os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                                  'www', 'sql.png'),
+                constructors=(plugin.manage_addSqlalchemyPlugin,),
+                visibility=None
+            )
