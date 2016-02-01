5c5e67138bce4461b9bea7e36ed802f12f0015cc
Luca Fabbri <luca@keul.it>
2016-02-01 15:47:23 +0100

    pypi classifiers

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requires = [
     'zope.sqlalchemy',
     ]
 
-tests_requires = requires + ['pysqlite']
+tests_requires = requires + ['pysqlite', ]
 
 setup(
     name='pas.plugins.sqlalchemy',
@@ -26,6 +26,9 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
+        "Framework :: Zope2",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
         "Programming Language :: Python",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI",
