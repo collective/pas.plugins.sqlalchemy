f6989fd5a210346c8c525847e40058ff233225f1
Luca Fabbri <luca@keul.it>
2016-02-01 19:10:26 +0100

    flake8 cleanup

--- src/pas/plugins/sqlalchemy/__init__.py
+++ src/pas/plugins/sqlalchemy/__init__.py
@@ -13,11 +13,11 @@ def initialize(context):
     if plugin.Plugin.meta_type not in MultiPlugins:
         registerMultiPlugin(plugin.Plugin.meta_type)
         context.registerClass(
-                plugin.Plugin,
-                permission=add_user_folders,
-                icon=os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                                  'www', 'sql.png'),
-                constructors=(plugin.manage_addSqlalchemyPluginForm,
-                              plugin.addSqlalchemyPlugin),
-                visibility=None
-            )
+            plugin.Plugin,
+            permission=add_user_folders,
+            icon=os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                              'www', 'sql.png'),
+            constructors=(plugin.manage_addSqlalchemyPluginForm,
+                          plugin.addSqlalchemyPlugin),
+            visibility=None
+        )
